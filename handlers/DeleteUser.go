8b430bfff1b4ed5fbaa1d605a943f9801389ea95
Aelpxy <aelpxy@protonmail.com>
2022-03-05 15:51:00 +0600

    deleted user?

--- handlers/DeleteUser.go
+++ handlers/DeleteUser.go
@@ -19,7 +19,7 @@ func DeleteUser(w http.ResponseWriter, r *http.Request) {
 
 			w.Header().Add("content-type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode("deleted")
+			json.NewEncoder(w).Encode("deleted user")
 			break
 		}
 	}
