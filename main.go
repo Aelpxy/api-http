14860ba2417da55fb902cbca1a5887c255255780
Aelpxy <aelpxy@protonmail.com>
2022-03-08 23:35:48 +0600

    spaces

--- main.go
+++ main.go
@@ -14,8 +14,11 @@ func main() {
 	router.HandleFunc("/u", handlers.GetAllUsers).Methods(http.MethodGet)
 
 	router.HandleFunc("/u/{id}", handlers.GetUser).Methods(http.MethodGet)
+
 	router.HandleFunc("/u", handlers.PostUser).Methods(http.MethodPost)
+
 	router.HandleFunc("/u/{id}", handlers.DeleteUser).Methods(http.MethodDelete)
+
 	router.HandleFunc("/u/{id}", handlers.PutUser).Methods(http.MethodPut)
 
 	log.Println("Listening on PORT 8080")
